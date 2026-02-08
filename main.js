d7727c7af8cc0a9eebe024ef741512a8a19d0b3a
Qwixysis <nazarkoval32139@gmail.com>
2026-02-08 21:25:59 +0200

    main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import { auth } from "./firebase.js";
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { getFirestore, doc, onSnapshot, updateDoc, arrayUnion } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const db = getFirestore();
@@ -16,5 +16,10 @@ onAuthStateChanged(auth, (user) => {
         document.getElementById("userNick").innerText = d.nick || "Jarvis";
         document.getElementById("userUid").innerText = user.uid;
         document.getElementById("userAvatarMain").innerText = (d.nick || "J")[0];
+        document.getElementById("pAvatar").innerText = (d.nick || "J")[0];
     });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById("logoutBtn").onclick = () => signOut(auth);
 });
